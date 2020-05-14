abc534c2cb45472a680b8f079222bd00bd49778d
Andrew Byrne <abyrne@myhome.ie>
2020-05-14 14:56:39 +0100

    Added success message

--- src/app/profile-settings/profile-settings.component.ts
+++ src/app/profile-settings/profile-settings.component.ts
@@ -12,7 +12,7 @@ export class ProfileSettingsComponent implements OnInit {
   error: boolean = false;
   user: IProfile;
   errorMessage: string;
-
+  updateSuccesful: boolean;
   constructor(private _profileSettingsService: ProfileSettingsService) {}
 
   ngOnInit() {
@@ -32,10 +32,13 @@ export class ProfileSettingsComponent implements OnInit {
   async saveUser() {
     this.savingProfile = true;
     this.errorMessage = null;
+    this.updateSuccesful = false;
+
     try {
       await this._profileSettingsService.setName(this.user);
       this.user = await this._profileSettingsService.setUserEmail();
       this.savingProfile = false;
+      this.updateSuccesful = true;
     } catch (error) {
       this.savingProfile = false;
       this.errorMessage = error.message;
